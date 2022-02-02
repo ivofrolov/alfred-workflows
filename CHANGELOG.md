ecd4947f0db62c30b199add9bbb4d2b615df2a99
Ivan Frolov <ivofrolov@fastmail.com>
2022-02-02 21:31:58 +0300

    Adds Yandex Dictionary support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+- [ ] Exact time support (to be able to run session until 18:00, 6pm etc.) in Amphetamine Commander
+- [x] Yandex Dictionary support https://yandex.ru/dev/dictionary/doc/dg/reference/lookup.html
+
+
 # v2
 
 Wolfram Answers workflow simplified (now using just curl).
--- yandex-translate/translate.py
+++ yandex-translate/translate.py
@@ -3,33 +3,102 @@
 import argparse
 import json
 import os
+from typing import Iterable, Iterator, NamedTuple
 from urllib.error import HTTPError
+from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
 
 TRANSLATE_API_URL = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
+DICTIONARY_API_URL = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup'
 
 
-def alfred_output(query: str, result: str) -> str:
-    return json.dumps(
+class DictionaryError(Exception):
+    ...
+
+
+class DictionaryResult(NamedTuple):
+    definition: str
+    description: str = ''
+    addition: str = ''
+
+    def __str__(self) -> str:
+        result = self.definition
+        if self.description:
+            result += f' ({self.description})'
+        if self.addition:
+            result += f'\n{self.addition}'
+        return result
+
+
+def alfred_output(query: str, results: Iterable[DictionaryResult]) -> str:
+    items = []
+
+    for result in results:
+        title = result.definition
+        if result.description:
+            title += f' ({result.description})'
+
+        largetype = title
+        if result.addition:
+            largetype += f'\n\n{result.addition}'
+
+        items.append(
+            {
+                'arg': query,
+                'title': title,
+                'subtitle': result.addition,
+                'text': {'copy': result.definition, 'largetype': largetype},
+            }
+        )
+
+    return json.dumps({'items': items}, ensure_ascii=False)
+
+
+def alfred_error(query: str, message: str) -> str:
+    items = [
         {
-            'items': [
-                {
-                    'title': result,
-                    'arg': query,
-                    'text': {'copy': result, 'largetype': result},
-                }
-            ]
+            'arg': query,
+            'title': message,
+            'text': {'copy': message, 'largetype': message},
         },
-        ensure_ascii=False,
+    ]
+    return json.dumps({'items': items}, ensure_ascii=False)
+
+
+def lookup(
+    word: str, source_lang: str, target_lang: str, *, api_key: str
+) -> Iterator[DictionaryResult]:
+    query = urlencode(
+        {'text': word, 'lang': f'{source_lang}-{target_lang}', 'key': api_key}
     )
+    try:
+        with urlopen(f'{DICTIONARY_API_URL}?{query}') as response:
+            dict_result = json.load(response)
+            for definition in dict_result['def']:
+                for translation in definition['tr']:
+                    means = [mean['text'] for mean in translation.get('mean', [])]
+                    synonyms = [
+                        synonym['text'] for synonym in translation.get('syn', [])
+                    ]
+                    yield DictionaryResult(
+                        definition=translation['text'],
+                        description=', '.join(means),
+                        addition=', '.join(synonyms),
+                    )
+    except HTTPError as exc:
+        error = json.load(exc)
+        raise DictionaryError(error['message'])
 
 
-def translate(text: str, target_lang: str, *, api_key: str) -> str:
+def translate(
+    text: str, source_lang: str, target_lang: str, *, api_key: str
+) -> Iterator[DictionaryResult]:
     request = Request(
         url=TRANSLATE_API_URL,
         data=json.dumps(
             {
+                'sourceLanguageCode': source_lang,
                 'targetLanguageCode': target_lang,
                 'texts': [text],
             }
@@ -41,9 +110,10 @@ def translate(text: str, target_lang: str, *, api_key: str) -> str:
     )
     try:
         with urlopen(request) as response:
-            return json.load(response)['translations'][0]['text']
+            text = json.load(response)['translations'][0]['text']
+            yield DictionaryResult(definition=text)
     except HTTPError as error:
-        return f'Error: {error.reason}'
+        raise DictionaryError(f'Error: {error.reason}')
 
 
 if __name__ == '__main__':
@@ -53,16 +123,34 @@ if __name__ == '__main__':
     parser.add_argument(
         '--raw', action='store_true', help='print result as plain text.'
     )
+    parser.add_argument('-s', '--source', help='source language code (en, ru, ...)')
     parser.add_argument('-t', '--target', help='target language code (en, ru, ...)')
     parser.add_argument('text')
 
     args = parser.parse_args()
 
-    answer = translate(
-        args.text, args.target, api_key=os.getenv('YANDEX_TRANSLATE_API_KEY', '')
-    )
-
-    if args.raw:
-        print(answer)
+    try:
+        if len(args.text.split()) == 1:
+            answers = lookup(
+                args.text,
+                args.source,
+                args.target,
+                api_key=os.getenv('YANDEX_DICTIONARY_API_KEY', ''),
+            )
+        else:
+            answers = translate(
+                args.text,
+                args.source,
+                args.target,
+                api_key=os.getenv('YANDEX_TRANSLATE_API_KEY', ''),
+            )
+    except DictionaryError as error:
+        if args.raw:
+            print(error)
+        else:
+            print(alfred_error(args.text, str(error)))
     else:
-        print(alfred_output(args.text, answer))
+        if args.raw:
+            print('\n\n'.join(map(str, answers)))
+        else:
+            print(alfred_output(args.text, answers))
